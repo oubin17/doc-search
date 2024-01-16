b6cdaa165807bc10eb69ad3036533aa096515e94
oubin <oubin17@gmail.com>
2024-01-16 22:21:42 +0800

    读写doc文件

---
+++ core/manager/src/main/java/com/odk/odktemplatemanager/util/FileUtil.java
@@ -0,0 +1,95 @@
+package com.odk.odktemplatemanager.util;
+
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.ooxml.POIXMLDocument;
+import org.apache.poi.ooxml.extractor.POIXMLTextExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+
+/**
+ * FileUtil
+ * 处理docx：https://blog.csdn.net/m0_63270887/article/details/132871594
+ *
+ * @description:
+ * @version: 1.0
+ * @author: oubin on 2024/1/16
+ */
+public class FileUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+
+    /**
+     * 保存图片到服务器
+     *
+     * @param filePath
+     * @param input
+     * @return
+     * @throws IOException
+     */
+    public static String saveFile(String filePath, String fileId, String fileName, InputStream input) throws IOException {
+
+        File file = new File(filePath);
+        if (!file.getParentFile().exists()) {
+            file.mkdirs();
+        }
+        filePath = filePath + generateFileName(fileId, fileName);
+        OutputStream out = new FileOutputStream(filePath);
+        byte[] data = new byte[2048];
+        int len = 0;
+        while ((len = input.read(data)) != -1) {
+            out.write(data, 0, len);
+        }
+        if (input != null) {
+            input.close();
+        }
+        out.close();
+        return filePath;
+    }
+
+    /**
+     * 获取文件内容
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public static String getDocContents(String filePath, String fileId, String fileName) throws IOException {
+
+        filePath = filePath + generateFileName(fileId, fileName);
+        String buffer = "";
+        try {
+            if (filePath.endsWith(".doc")) {
+                InputStream is = Files.newInputStream(new File(filePath).toPath());
+                WordExtractor ex = new WordExtractor(is);
+                buffer = ex.getText();
+                ex.close();
+            } else if (filePath.endsWith(".docx")) {
+                OPCPackage opcPackage = POIXMLDocument.openPackage(filePath);
+                POIXMLTextExtractor extractor = new XWPFWordExtractor(opcPackage);
+                buffer = extractor.getText();
+                extractor.close();
+            }
+        } catch (Exception e) {
+            logger.error("获取文件内容发生异常：", e);
+
+        }
+        return buffer;
+    }
+
+    /**
+     * 文件名
+     *
+     * @param fileId
+     * @param fileName
+     * @return
+     */
+    private static String generateFileName(String fileId, String fileName) {
+        return fileId + "_" + fileName;
+    }
+}
--- core/service/src/main/java/com/odk/odktemplateservice/impl/DocServiceImpl.java
+++ core/service/src/main/java/com/odk/odktemplateservice/impl/DocServiceImpl.java
@@ -1,10 +1,13 @@
 package com.odk.odktemplateservice.impl;
 
+import com.odk.base.exception.BizErrorCode;
+import com.odk.base.exception.BizException;
+import com.odk.odktemplatemanager.util.FileUtil;
 import com.odk.odktemplateservice.DocService;
 import com.odk.template.domain.domain.Doc;
 import com.odk.template.domain.impl.DocRepository;
-import com.odk.template.util.common.FileUtil;
 import com.odk.template.util.dto.DocSaveDto;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,20 +37,27 @@ public class DocServiceImpl implements DocService {
     @Override
     public String saveDoc(DocSaveDto docSaveDto) {
         String docId = UUID.randomUUID().toString();
-        //1.上传文件
         try {
+            //1.上传文件
             FileUtil.saveFile(filePath, docId, docSaveDto.getDocName(), docSaveDto.getFileInputStream());
+            //2.获取文件内容
+            String docContents = FileUtil.getDocContents(filePath, docId, docSaveDto.getDocName());
+            logger.info("文件内容 {}", docContents);
+            if (StringUtils.isNotEmpty(docContents)) {
+                //3.内容写到ES
+            }
         } catch (IOException e) {
             logger.error("文件上传失败，文件名：{}", docSaveDto.getDocName());
-            throw new RuntimeException(e);
+            throw new BizException(BizErrorCode.SYSTEM_ERROR);
+        } catch (Exception e) {
+            logger.error("文件上传发生未知错误,", e);
+            throw e;
         }
         //2.保存文件到db
         Doc doc = new Doc();
         doc.setDocId(docId);
         doc.setDocName(docSaveDto.getDocName());
         docRepository.saveDoc(doc);
-        //3.上传到ES
-
         return docId;
     }
 
